5c536091f41d1892bbb3b952607dfd968ab79d69
Gobind Puniani <gobindpuniani@gmail.com>
2021-10-04 03:01:40 -0700

    Can almost broadcast user disconnects

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        io.emit('user disconnected');
     });
 
     socket.on('chat message', (msg) => {
